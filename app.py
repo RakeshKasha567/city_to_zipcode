8baa12c125a0982a5d45d5a2aeabcbdb57812e7a
RKasha <124204981+RakeshKasha567@users.noreply.github.com>
2023-02-09 17:26:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -36,15 +36,25 @@ def get_zipcode(city_name):
 def add_zipcode():
     city = request.json['city']
     zip_code = request.json['zip_code']
-    res = requests.get("http://172.17.0.2:5004")
 
-    new_zip_code = {
-        "city": city,
-        "zip_code": zip_code
-    }
+    # Make a request to the weather microservice
+    weather_url = f"http://172.17.0.2:5004/{zip_code}" 
+    weather_response = request.get(weather_url)
+    
+    # Check the response status code
+    if weather_response.status_code == 200:
+        weather_data = weather_response.json()
+        new_zip_code = {
+            "city": city,
+            "zip_code": zip_code,
+            "weather": weather_data
+        }
+    else:
+        return "Error retrieving weather data", weather_response.status_code
 
     zip_codes.append(new_zip_code)
-    return jsonify({"message": "Zip code added"})
+    return jsonify({"message": "Zip code and weather data added"})
+
 
 if __name__ == '__main__':
     
